b53ebb540cfc32a0c615e6d375829356f0ea6083
fserk729 <fabian.serkeyn@hotmail.com>
2025-06-08 16:14:47 +0200

    tf infra wf

--- mlops-course-04/terraform/modules/s3-bucket/main.tf
+++ mlops-course-04/terraform/modules/s3-bucket/main.tf
@@ -1,4 +1,8 @@
+resource "random_id" "suffix" {
+  byte_length = 4
+}
+
 resource "aws_s3_bucket" "s3" {
-  bucket = var.bucket
+  bucket = "${var.bucket}-${random_id.suffix.hex}"
   tags   = var.tags
 }
